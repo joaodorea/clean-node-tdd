114cd5da3e908f2d6d12f678a50cdedb86330dfd
Joao Dorea <joao.dorea@contabilizei.com.br>
2020-08-03 23:00:41 -0300

    chore: add jest

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+export default {
+  root: ['<rootDir>/src'],
+  clearMocks: true,
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coverageDirectory: 'coverage',
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  }
+}
